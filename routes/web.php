76b609983fef3e4f9453bf0416a2064e52265438
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-14 21:55:31 +0600

    Refactor domain routes to use configuration for multi-domain support

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::domain('domain1.test')->group(function () {
+// Route::domain('domain1.test')->group(function () {
+//     Route::get('/', [App\Http\Controllers\Domain1\HomeController::class, 'index']);
+// });
+
+// Route::domain('domain2.test')->group(function () {
+//     Route::get('/', [App\Http\Controllers\Domain2\HomeController::class, 'index']);
+// });
+
+// Route::domain('domain3.test')->group(function () {
+//     Route::get('/', [App\Http\Controllers\Domain3\HomeController::class, 'index']);
+// });
+
+Route::domain(config('app.domains.domain1'))->group(function () {
     Route::get('/', [App\Http\Controllers\Domain1\HomeController::class, 'index']);
+
 });
 
-Route::domain('domain2.test')->group(function () {
-    Route::get('/', [App\Http\Controllers\Domain2\HomeController::class, 'index']);
+Route::domain(config('app.domains.domain2'))->group(function () {
+    Route::get('/', [App\Http\Controllers\Domain1\HomeController::class, 'index']);
+
 });
 
-Route::domain('domain3.test')->group(function () {
-    Route::get('/', [App\Http\Controllers\Domain3\HomeController::class, 'index']);
+Route::domain(config('app.domains.domain3'))->group(function () {
+    Route::get('/', [App\Http\Controllers\Domain1\HomeController::class, 'index']);
+
 });

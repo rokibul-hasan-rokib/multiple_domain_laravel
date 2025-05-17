f16c04334a6dcfc1aa5182ed8fd1c012e9f67d4c
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-17 21:45:57 +0600

    Refactor Order model to include payment constants and improve structure

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    use HasFactory;
+    protected $guarded = [];
+
+    public const DEFAULT_TRIAL_DAYS = 7;
+
+    public const PAYMMENT_PENDING = 2;
+    public const PAYMENT_COMPELTED = 1;
+
+    public const PAYMENT_METHOD_STRIPE      = 1;
+    public const PAYMENT_METHOD_PAYPAL      = 2;
+    public const PAYMENT_METHOD_PAYGATE     = 3;
+    public const PAYMENT_METHOD_READIES     = 4;
+    public const PAYMENT_METHOD_NOWPAYMENTS = 5;
+
+    public const PAYMENT_METHOD_LIST = [
+        self::PAYMENT_METHOD_STRIPE      => 'Stripe',
+        self::PAYMENT_METHOD_PAYGATE     => 'PayGate',
+        self::PAYMENT_METHOD_READIES     => 'Readies',
+        self::PAYMENT_METHOD_NOWPAYMENTS => 'NowPayments',
+    ];
 }

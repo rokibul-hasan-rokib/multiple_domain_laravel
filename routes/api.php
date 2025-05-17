e9be6d524f8bed7e188606ce654418c7d34ce329
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-17 19:31:11 +0600

    Refactor API routes to improve organization and readability; group payment-related routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\StripeApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('pay-gate/{order}', [StripeApiController::class, 'payGate']);
+Route::get('paygate-callback', [StripeApiController::class, 'paygateCallback'])->name('paygate.callback');
+
+
+Route::get('stripe', [StripeApiController::class, 'createCheckout']);
+Route::post('stripe-webhook/success', [StripeApiController::class, 'webhookSuccess']);
+Route::post('stripe-webhook/invoice-paid', [StripeApiController::class, 'invoicePaid']);

f772bff18fddc13d4014ee63b965cecc7838d688
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-17 23:17:46 +0600

    Refactor Order model to enhance data preparation methods and improve order management

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class Order extends Model
 {
@@ -27,4 +28,37 @@ class Order extends Model
         self::PAYMENT_METHOD_READIES     => 'Readies',
         self::PAYMENT_METHOD_NOWPAYMENTS => 'NowPayments',
     ];
+
+    final public function prepare_data(Request $request, Package $package, User $user)
+    {
+        return [
+            'company_id'     => $request->header('company_id'),
+            'package_id'     => $package->id,
+            'user_id'        => $user->id,
+            'price'          => $package->price,
+            'currency'       => Package::DEFAULT_CURRENCY,
+            'discount'       => $request->input('discount', 0),
+            'total'          => $package->price - $request->input('discount', 0),
+            'status'         => self::STATUS_PENDING,
+            'payment_status' => self::PAYMENT_STATUS_PENDING,
+            'payment_method' => $request->input('payment_method', self::PAYMENT_METHOD_STRIPE),
+        ];
+    }
+
+
+    final public function store_data(Request $request, Package $package, User | Authenticatable $user): Builder | Model
+    {
+        $order = self::query()->create($this->prepare_data($request, $package, $user));
+        $order->order_detail()->create((new OrderDetail())->prepare_data($package, $order));
+        // (new CompanyPackage())->storeCompanyPackage($order);
+        return $order;
+    }
+
+    final public function updateStatus(Order $order, int $status, int $payment_status): bool
+    {
+        return $order->update([
+            'status'         => $status,
+            'payment_status' => $payment_status,
+        ]);
+    }
 }

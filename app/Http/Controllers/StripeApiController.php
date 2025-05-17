37a6ea4e796ce273cae788908525462880f0c443
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2025-05-17 19:29:57 +0600

    Implement invoicePaid method to handle invoice payment webhooks and update transaction status

--- app/Http/Controllers/StripeApiController.php
+++ app/Http/Controllers/StripeApiController.php
@@ -110,6 +110,35 @@ class StripeApiController extends Controller
     }
 
 
+    final public function invoicePaid(Request $request)
+    {
+        Log::info('INVOICE_PAID_WEBHOOK_RECEIVED', ['data' => $request->all()]);
+        try {
+            DB::beginTransaction();
+            $response = (new StripeManager())->getInvoice($request->data['object']['id'] ?? null);
+            if ($response?->status === 'paid') {
+                Log::info('INVOICE_PAID_PAYMENT_PAID', ['response' => $response]);
+                $transaction = (new PaymentTransaction())->getByColumn('invoice_id', $response->id);
+                if (!$transaction) {
+                    Log::error('INVOICE_PAID_API_ERROR', ['message' => 'Payment transaction not found', 'tran_id' => $response->id, 'request' => $request->all(), 'response' => $response]);
+                    return response()->json(['message' => 'Payment transaction not found'], 404);
+                }
+                $transaction->load(['order']);
+                (new PaymentTransaction())->updateSuccess($request, $transaction);
+                (new Order())->updateStatus($transaction->order, Order::STATUS_COMPLETED, Order::PAYMENT_STATUS_PAID);
+                (new CompanyPackage())->storeCompanyPackage($transaction->order);
+            } else {
+                Log::error('INVOICE_PAID_PAYMENT_NOT_PAID', ['response' => $response, 'request' => $request->all()]);
+                return response()->json(['message' => 'Payment not paid'], 400);
+            }
+            DB::commit();
+        } catch (Throwable $e) {
+            DB::rollBack();
+            Log::error('ERROR_WHILE_UPDATING_PAYMENT_ATTEMPT', ['message' => $e->getMessage(), 'tran_id' => $request->data['object']['id'], 'data' => $request->all()]);
+            return response()->json(['message' => 'Error while updating payment attempt'], 500);
+        }
 
+        return response()->json(['message' => 'Payment success webhook received']);
+    }
 
 }
